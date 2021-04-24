fb3f92a064c2cba24e13bf5879c411c0375d2196
Sanjeev Sritharan <35245440+sanjeev2001@users.noreply.github.com>
2021-04-24 17:28:34 -0400

    speed and unpaid update correctly

--- serialSend.py
+++ serialSend.py
@@ -3,37 +3,40 @@ import time
 
 import json
 
-ser = serial.Serial('COM3', 9600, timeout = 5)
+ser = serial.Serial('COM3', 9600, timeout=5)
 print(ser.name)
 
 while(True):
-  time.sleep(2) # wait for Arduino
+    time.sleep(2)  # wait for Arduino
 
-  with open('data.json') as f:
-    data = json.load(f)
+    with open('data.json') as f:
+        data = json.load(f)
 
-  speed = data['devices'][0]['speeds'][0]['speed']
-  unpaid = data['unpaidAmount'][1:]
-  fixedSpeed = speed + " "*(5-len(speed))
-  fixedUnpaid = unpaid + " "*(9-len(unpaid))
- 
-  print(speed)
-  print(unpaid)
-  print(fixedSpeed)
-  print(fixedUnpaid)
-  
-  ser.flush()
-  if(len(speed) == 5):
+    speed = data['devices'][0]['speeds'][0]['speed']
+    unpaid = data['unpaidAmount'][1:]
+    # speed = "30"
+    # unpaid = ".0000101"
+    if(len(speed) != 5):
+        speed = speed + " "*(5-len(speed))
+    if (len(unpaid) != 9):
+        unpaid = unpaid + " "*(9-len(unpaid))
+    # fixedSpeed = speed + "1"*(5-len(speed))
+    # fixedUnpaid = unpaid + "1"*(9-len(unpaid))
+
+    print("Speed: " + speed)
+    print("Unpaid: " + unpaid)
+
+    ser.flush()
+    # if(len(speed) == 5):
     ser.write(str(speed).encode('ascii'))
-  else:
-    ser.write(str(speed + fixedSpeed).encode('ascii'))
-    
-  if (len(unpaid) == 9):
+    # else:
+    #     ser.write(str(speed + "1"*(5-len(speed))).encode('ascii'))
+    time.sleep(1)
+    # if (len(unpaid) == 9):
     ser.write(str(unpaid).encode('ascii'))
-  else:
-    ser.write(str(unpaid + fixedUnpaid).encode('ascii'))
-  time.sleep(5)
-  ser.flush()
+    # else:
+    #     ser.write(str(unpaid + fixedUnpaid).encode('ascii'))
+    time.sleep(3)
+    ser.flush()
 
 exit()
-
